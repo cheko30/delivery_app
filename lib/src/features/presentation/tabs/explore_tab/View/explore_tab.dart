cfebd32ffd943c246dbf52b5a0da0ee327a814ef
sergio.pena <sergio.pena.orozco@gmail.com>
2025-08-31 22:36:55 -0600

    add slider cards and slider collections

--- lib/src/features/presentation/tabs/explore_tab/View/explore_tab.dart
+++ lib/src/features/presentation/tabs/explore_tab/View/explore_tab.dart
@@ -23,7 +23,14 @@ class ExploreTab extends StatelessWidget {
                       alignment: Alignment.centerLeft,
                       child: headerText('Discover new places', Colors.black, FontWeight.bold, 30.0)
                     ),
-                    _sliderCards(context)
+                    _sliderCards(context),
+                    _headers(context, 'Popular this week', 'Show all'),
+                    _populars(context, 'https://images.unsplash.com/photo-1581546104493-f7e013a136ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'),
+                    _populars(context, 'https://images.unsplash.com/photo-1717588874169-da2ba5ec3ec2?q=80&w=855&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+                    _populars(context, 'https://images.unsplash.com/photo-1505394033641-40c6ad1178d7?q=80&w=887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+                    SizedBox(height: 10.0,),
+                    _headers(context, "Collections", "Show all"),
+                    _sliderCollections(context),
                   ],
                 ),
               )
@@ -189,3 +196,164 @@ Widget _card(BuildContext context) {
     ),
   );
 }
+
+Widget _headers(BuildContext context, String textHeader, String textAction) {
+  return Row(
+    children: [
+      Container(
+        alignment: Alignment.centerLeft,
+        child: headerText(textHeader, Colors.black, FontWeight.bold, 20.0),
+      ),
+      Spacer(),
+      GestureDetector(
+        child: Row(
+          children: [
+            Text(
+              textAction,
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+                fontSize: 15.0
+              ),
+            ),
+            Icon(Icons.play_arrow)
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+Widget _populars(BuildContext context, String photo) {
+  return Column(
+    children: [
+      Container(
+        margin: EdgeInsets.only(left: 10.0),
+        padding: EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: Image(
+                width: 80.0,
+                height: 80.0,
+                fit: BoxFit.cover,
+                image: NetworkImage(photo)
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 7.0),
+                    child: headerText("Andy & Cindy's Dinner", Colors.black, FontWeight.bold, 17.0)
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    margin: EdgeInsets.only(bottom: 5.0),
+                    child: Text(
+                      "87 Botsford Circle Apt",
+                      style: TextStyle(
+                        color: gray,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 13.0
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.star, color: yellow, size: 16.0,),
+                      Text(
+                        "4.5",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 13.0
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 5.0),
+                        child: Text(
+                          "233 ratings",
+                          style: TextStyle(
+                            color: gray,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 13.0
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 35.0),
+                        width: 110.0,
+                        height: 18.0,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            
+                          },
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0.5,
+                            shape: StadiumBorder(),
+                            backgroundColor: Color.fromRGBO(255, 140, 0, 1.0),
+                          ),
+                          child: Text(
+                            'Delivery',
+                            style: TextStyle(
+                              color: Colors.white,                                        
+                              fontSize: 11.0
+                            ),
+                          ),
+              
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      )
+    ],
+  );
+}
+
+Widget _sliderCollections(BuildContext context) {
+  return Container(
+    height: 180.0,
+    child: Swiper(
+      itemCount: 4,
+      layout: SwiperLayout.DEFAULT,
+      itemBuilder: (BuildContext context, int index) {
+        return ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context, int index) {
+            return _cardCollections(context);
+          }
+        );
+      },
+    ),
+  );
+}
+
+Widget _cardCollections(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image(
+              width: 300.0,
+              height: 150.0,
+              fit: BoxFit.cover,
+              image: NetworkImage("https://images.unsplash.com/photo-1565958011703-44f9829ba187?q=80&w=930&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+            ),
+          )
+        ],
+      ),
+    );
+  }
